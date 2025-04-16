dc68df258ac8d05cf7118068822ba9e7ffde9e5b
Uniaut <kunwoo0927@gmail.com>
2025-04-17 00:25:04 +0900

    implement parse_text

--- valuator/utils/llm_utils.py
+++ valuator/utils/llm_utils.py
@@ -1,14 +1,49 @@
 import json
 import os
 
-from langchain_community.chat_models import ChatPerplexity
+from langchain_perplexity import ChatPerplexity
 from langchain_openai import ChatOpenAI
 
 
-def parse_text(text: str, key_and_description: dict[str, str]) -> dict[str, str]:
-    result = '{"name": "Alice", "age": 30, "is_student": false}'
-    d = json.loads(result)
+gpt_41 = None
+pplx = None
+
+def init_llms():
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    pplx_api_key = os.getenv("PPLX_API_KEY")
     
+    global gpt_41, pplx
+
+    if openai_api_key:
+        gpt_41 = ChatOpenAI(api_key=openai_api_key)
+    else:
+        gpt_41 = None
+
+    if pplx_api_key:
+        pplx = ChatPerplexity(api_key=pplx_api_key)
+    else:
+        pplx = None
+
+
+def parse_text(text: str, key_and_description: dict[str, str]) -> dict[str, str]:
+    result_json = gpt_41.invoke(
+f'''
+based on full text, parse informations as json.
+text:
+{text}
+keys & description:
+{key_and_description}
+'''
+    ).content
+
+    try:
+        d = json.loads(result_json)
+    except Exception:
+        print('parsing error')
+        print('raw output:', result_json)
+        d = key_and_description
+    finally: 
+        print('parsed:', d)
     return d
 
 

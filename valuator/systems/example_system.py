10c798f5b7a76e081500f9b7614ba7f33c8ebadc
Uniaut <kunwoo0927@gmail.com>
2025-04-20 16:46:43 +0900

    rename

---
+++ valuator/systems/example_system.py
@@ -0,0 +1,12 @@
+from Valuator.valuator.modules import example_module
+
+def subsystem_example_dating_corps():
+    match_group_analysis = example_module.run('match group')
+    grindr_analysis = example_module.run('grindr')
+
+    return (match_group_analysis, grindr_analysis)
+
+
+def run():
+    adsf = subsystem_example_dating_corps()
+    return adsf

3c0774312615e5efc73f07f7ac21766465017d56
Uniaut <kunwoo0927@gmail.com>
2025-06-15 16:53:38 +0900

    resolve running issue

--- valuator/utils/finsource/collector.py
+++ valuator/utils/finsource/collector.py
@@ -35,7 +35,7 @@ def fetch_using_readerLLM(corp: str, url: str):
         if (clean_line := parse_and_clean_markdown_table(line))
     ]
     with open(
-        f"./utils/finsource/data/{corp}-10-k.html", "w", encoding="utf-8"
+        f"valuator/utils/finsource/data/{corp}-10-k.html", "w", encoding="utf-8"
     ) as file:
         file.write("\n".join(text))
     return text
